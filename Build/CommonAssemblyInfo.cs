1414615f8e7479f706530c08aeaf800a93a2e8ac
Chris Hynes <chris@programcsharp.com>
2014-02-28 11:48:07 -0700

    Updated to m3 compat

---
+++ Build/CommonAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Griddly")]
+[assembly: AssemblyCopyright("Copyright © 2014 Chris Hynes and Data Research Group")]
+
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyFileVersion("1.0.11.0")]
