c23f8f299eba8b88cffdb18114111da969098398
Chris Hynes <chris@programcsharp.com>
2014-11-26 13:42:09 -0700

    Reskin header Add getting started example Move other stuff to secondary pages

---
+++ Griddly/Models/SimplePerson.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Griddly.Models
+{
+    public class SimplePerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
