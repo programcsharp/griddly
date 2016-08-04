35134f9838e09349d97bf69b4e40ec4f69f73cc0
Chris Hynes <chris@programcsharp.com>
2016-08-04 07:33:52 -0700

    Version bump

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.7")]
-[assembly: AssemblyFileVersion("1.5.7")]
+[assembly: AssemblyVersion("1.5.8")]
+[assembly: AssemblyFileVersion("1.5.8")]
 //[assembly: AssemblyInformationalVersion("1.4.5-editlyalpha2")]
