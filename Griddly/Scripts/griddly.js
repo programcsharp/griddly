dd3842dfe5b436f46dd1b67988520414857f4245
Chris Hynes <chris@programcsharp.com>
2017-05-02 16:43:00 -0700

    Don't asplode in jQuery 3

--- Griddly/Scripts/griddly.js
+++ Griddly/Scripts/griddly.js
@@ -1007,7 +1007,7 @@
             this.setFilterValues(this.options.filterDefaults, null, true);
 
             // clear any none's that were inadvertently reset
-            this.$element.find(".griddly-filters-form [data-griddly-filter-isnoneall=true] [multiple] option[value=]").prop("selected", false);
+            this.$element.find(".griddly-filters-form [data-griddly-filter-isnoneall=true] [multiple] option[value='']").prop("selected", false);
 
             this.triggerOrQueue(this.$element, "resetfilters.griddly");
 
