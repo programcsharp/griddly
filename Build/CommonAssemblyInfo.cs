36489315c658ca1a52b15c9392ebe60340e7aa97
Chris Hynes <chris@programcsharp.com>
2017-12-23 06:19:40 -0700

    Release v2.0

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.9.3")]
-[assembly: AssemblyFileVersion("1.9.3")]
-[assembly: AssemblyInformationalVersion("2.0-alpha6")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+//[assembly: AssemblyInformationalVersion("2.0-alpha6")]
