745cc5d4fa92be85b140381879bc66a594d7ac30
Jon Hynes <jonh@datarg.com>
2022-01-24 14:49:53 -0500

    It's 2022!

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Griddly")]
-[assembly: AssemblyCopyright("Copyright © 2013-2021 Chris Hynes, Joel Potter, and Data Research Group")]
+[assembly: AssemblyCopyright("Copyright © 2013-2022 Chris Hynes, Joel Potter, and Data Research Group")]
 
 [assembly: ComVisible(false)]
 
