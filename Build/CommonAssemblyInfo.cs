47d6b3da8b1d6c6b685030830801c7004deb0034
Jon Hynes <jonh@datarg.com>
2022-06-08 15:17:34 -0400

    Bump version

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.6.20")]
-[assembly: AssemblyFileVersion("3.6.20")]
+[assembly: AssemblyVersion("3.6.21")]
+[assembly: AssemblyFileVersion("3.6.21")]
 //[assembly: AssemblyInformationalVersion("2.5-filters")]
