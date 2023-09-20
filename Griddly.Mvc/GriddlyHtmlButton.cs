bd03f42fa691286cfc3d3aaa49231da4a2fd2e9c
Jon Hynes <jonh@datarg.com>
2023-09-20 15:38:51 -0400

    Make GriddlyHtmlButton be async in .net core

--- Griddly.Mvc/GriddlyHtmlButton.cs
+++ Griddly.Mvc/GriddlyHtmlButton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 #if NETCOREAPP
 using Microsoft.AspNetCore.Html;
@@ -6,14 +7,38 @@ using Microsoft.AspNetCore.Html;
 
 namespace Griddly.Mvc
 {
+#if NETCOREAPP
     public class GriddlyHtmlButton : GriddlyButton
     {
-        public Func<object,
-#if NETCOREAPP
-            IHtmlContent
-#else
-            object
-#endif
-            > HtmlTemplate { get; set; }
+        public GriddlyHtmlButton(Func<object, IHtmlContent> content) 
+        {
+            ContentTask = o => Task.FromResult(content(o));
+        }
+        public GriddlyHtmlButton(Task<IHtmlContent> content) 
+        {
+            ContentTask = o => content;
+        }
+
+        /// <summary>
+        /// Pass the HtmlTemplate in to the constructor instead of using this setter.
+        /// Kept for backward compatibility only.
+        /// </summary>
+        [Obsolete("Pass the HtmlTemplate in to the constructor")]
+        public Func<object, IHtmlContent> HtmlTemplate
+        {
+            set
+            {
+                var func = value;
+                ContentTask = o => Task.FromResult(func(o));
+            }
+        }
+
+        public Func<object, Task<IHtmlContent>> ContentTask { get; private set; }
     }
+#else
+    public class GriddlyHtmlButton : GriddlyButton
+    {
+        public Func<object, object> HtmlTemplate { get; set; }
+    }
+#endif
 }
