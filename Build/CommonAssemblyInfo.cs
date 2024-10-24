a6937291dfd7a2f9999165ac7f314a172e15fb05
Joel Potter <ithielnor@gmail.com>
2024-10-24 16:24:04 -0400

    Switch to using info version for nuget

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.7.8")]
-[assembly: AssemblyFileVersion("3.7.8-beta")]
-//[assembly: AssemblyInformationalVersion("2.5-filters")]
+[assembly: AssemblyFileVersion("3.7.8")]
+[assembly: AssemblyInformationalVersion("3.7.8-beta")]
