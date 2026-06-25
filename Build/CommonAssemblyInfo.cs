261398617ce30ec12b3d2b19d473b16c8d529137
Joel Potter <ithielnor@gmail.com>
2026-06-25 11:27:00 -0400

    Version bump 3.8.9

--- Build/CommonAssemblyInfo.cs
+++ Build/CommonAssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("3.8.8")]
-[assembly: AssemblyFileVersion("3.8.8")]
+[assembly: AssemblyVersion("3.8.9")]
+[assembly: AssemblyFileVersion("3.8.9")]
 // Uncomment the informational version to create a pre-release package
 //[assembly: AssemblyInformationalVersion("3.8.4-rc2")]
